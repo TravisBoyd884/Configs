a37f463a08aa5ff190983df4be6aadf4d5112672
TravisBoyd884 <phtboyd884@gmail.com>
2024-10-06 23:05:23 -0400

    Moved nvim config directory

---
+++ nvim/lua/travis/plugins/formatting.lua
@@ -0,0 +1,47 @@
+return {
+	"stevearc/conform.nvim",
+	lazy = true,
+	event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			filetypes = {
+				-- Exclude C++ files from conform.nvim
+				exclude = { "cpp" },
+			},
+			formatters_by_ft = {
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+				javascriptreact = { "prettier" },
+				typescriptreact = { "prettier" },
+				svelte = { "prettier" },
+				css = { "prettier" },
+				-- html = { "prettier" },
+				json = { "prettier" },
+				yaml = { "prettier" },
+				markdown = { "prettier" },
+				graphql = { "prettier" },
+				lua = { "stylua" },
+				cpp = { "clang-format" },
+				c = { "clang-format" },
+				python = { "isort", "black" },
+				tex = { "latexindent" },
+				asm = { "asmfmt" },
+			},
+			format_on_save = {
+				lsp_fallback = false,
+				async = false,
+				timeout_ms = 1000,
+			},
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
+}
